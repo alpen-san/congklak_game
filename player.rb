fa3dfdfbf78500cfcae547008f08f78a448c78b7
Muhammad Alpen Saputra Halim <alpenho@gmail.com>
2016-12-14 23:50:15 +0700

    Player - Add attribute name

--- player.rb
+++ player.rb
@@ -1,14 +1,15 @@
 class Player
-	attr_accessor :holes, :store_house, :status
+	attr_accessor :holes, :store_house, :status, :name
 
 	PLAYING = "playing"
 	WINNER = "winner"
 	LOSER = "loser"
 
-	def initialize
+	def initialize(name)
 		@holes = Array.new(7, 7)
 		@store_house = 0
 		@status = PLAYING
+		@name = name
 	end
 
 	def holes_empty?
